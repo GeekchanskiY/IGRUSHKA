7887b9af38ecf7d8651052ce67dd80060209d1d9
angelxhranitel <tima.seremyazhko@mail.ru>
2021-12-26 22:01:15 +0300

    фотографии, отрисовка

--- IGRUSHKA/Source.cpp
+++ IGRUSHKA/Source.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <Windows.h>
+#include "Header.h"
 
 using namespace sf;
 
@@ -14,47 +15,62 @@ void Key();
 
 // Объект, который, собственно, является главным окном приложения
 RenderWindow window(VideoMode(Width, Height), "Flappy Wiskey");
-Image image;
-Texture texture;
-Sprite sprite;
+Image wiskey, kristal, barrier;
+Texture texture, texturefon, texturetruba;
+Sprite bottle, truba, fon;
 
 int main()
 {
-    window.setFramerateLimit(60);
+	APP app;
+	window.setFramerateLimit(60);
 
-    //загрузка картинки из файла и в окно
-    image.loadFromFile("whiskey.png");
-    texture.loadFromImage(image);
-    sprite.setTexture(texture);
-    sprite.setPosition(50, 50);
+	//загрузка картинки бутылки
+	wiskey.loadFromFile("wiskey.png");
+	texture.loadFromImage(wiskey);
+	bottle.setTexture(texture);
+	bottle.setPosition(50, 50);
 
-    // Главный цикл приложения. Выполняется, пока открыто окно
-    while (window.isOpen())
-    {
-        // Обрабатываем очередь событий в цикле
-        Event event;
-        while (window.pollEvent(event))
-        {
-            // Пользователь нажал на «крестик» и хочет закрыть окно?
-            if (event.type == Event::Closed)
-                // тогда закрываем его
-                window.close();
-        }
+	//загрузка картинки фона
+	kristal.loadFromFile("fon.png");
+	texturefon.loadFromImage(kristal);
+	fon.setTexture(texturefon);
+	fon.setPosition(0,0);
 
-        Key();
+	//загрузка картинки преграды
+	kristal.loadFromFile("fon.png");
+	texturefon.loadFromImage(kristal);
+	fon.setTexture(texturefon);
+	fon.setPosition(0, 0);
 
-        // Отрисовка окна
-        window.display();
-        window.clear({ 255, 255, 255 });
 
-        window.draw(sprite);
-    }
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		// Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// тогда закрываем его
+				window.close();
+		}
 
-    return 0;
+		Key();
+		app.Tick();
+		// Отрисовка окна
+		window.display();
+		window.clear({ 255, 255, 255 });
+
+		window.draw(fon);
+		window.draw(bottle);
+	}
+
+	return 0;
 }
 
 void Key() {
-    if (Keyboard::isKeyPressed(Keyboard::Space)) {
-        sprite.move(Speed, 0);
-    }
+	if (Keyboard::isKeyPressed(Keyboard::Space)) {
+		bottle.move(Speed, 0);
+	}
 }
