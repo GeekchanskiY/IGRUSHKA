5415bdb7eb01454917df62a3aff5f1ed742eaede
Dmitriy <astrovskiy.04@gmail.com>
2021-12-27 00:54:18 +0300

    test kill logic

--- IGRUSHKA/Header.h
+++ IGRUSHKA/Header.h
@@ -17,7 +17,7 @@ public:
 			pos_x -= 5;
 			if (pos_x >= 100 && pos_x <= 280) {
 				std::cout << "I CAN KILL WOAHAHAHAH \n";
-				if (Whiskey_y <= -(pos_y + 100) || Whiskey_y >= -(pos_y - 100)) {
+				if (Whiskey_y >= 650 -(pos_y + 100) || Whiskey_y <= 650 -(pos_y - 100)) {
 					std::cout << "\n" << pos_y + 100 << "\n" << pos_y - 100 << "\n" << Whiskey_y;
 				}
 			}
